3edb162945c13558c58b6ca10795b53c6bce8972
artem431 <artemuhno62@gmail.com>
2021-10-21 13:42:00 +0300

    update file script.js and finished one tack

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const MIN_PRICE = 250000;
+const MAX_PRICE = 2000000;
+const MIN_SELLER_RATING = 0;
+const MAX_SELLER_RATING = 5;
+const ADDRESS_MIN_BUILDING = 1;
+const ADDRESS_MAX_BUILDING = 40;
+const MIN_AREA = 30;
+const MAX_AREA = 250;
+const MIN_ROOMS_COUNT = 1;
+const MAX_ROOMS_COUNT = 7;
+const MIN_IMAGE_COUNT = 1;
+const MAX_IMAGE_COUNT = 4;
+const COUNT_CARDS = 7;
+const CATEGORY = 'Недвижимость';
+
 var mySlider = new rSlider({
   target: '#sampleSlider',
   values: [10000, 1000000],
@@ -9,9 +24,6 @@ var mySlider = new rSlider({
   labels: false,
   step: 10000
 });
-
-const estateObject = 7;
-const category = 'Недвижимость';
 
 const nameArray = [
   'Двушка в центре Питера',
@@ -55,52 +67,44 @@ const fullNameArray = [
   'Марья Андреевна'
 ];
 
-const objectArray = [];
-const photos = [];
-
-
-for (let index = 0; index < getRandom(1, 4); index++) {
-  photos[index] = `img/${photosArray[getRandom(0, photosArray.length - 1)]}`;
-}
-
-for (let i = 0; i < estateObject; i++) {
-  objectArray.push(createObject());
-}
-
-function createObject() {
-  const object = {
-    name: nameArray[getRandom(0, nameArray.length - 1)],
-    description: descriptionArray[getRandom(0, descriptionArray.length - 1)],
-    price: getRandom (250000, 2000000),
-    category: category,
-    seller: {
-      fullname: fullNameArray[getRandom(0, fullNameArray.length - 1)],
-      rating: Math.ceil(getRandomFloat(0,5) * 10) / 10
-    },
-    photos: photos[getRandom(0, photos.length - 1)],
-    address: {
-      city: cityArray[getRandom(0, cityArray.length - 1)],
-      street: streetArray[getRandom(0, streetArray.length -1)],
-      building: getRandom(1, 40)
-    },
-    filters: {
-      type: typeArray[getRandom(0, typeArray.length - 1)],
-      area: getRandom(30, 250),
-      'rooms-count': getRandom(1, 7)
-    },
-    'publish-date': Date.now()
-  };
-  return object;
-}
-
-console.log(objectArray);
-
-
-function getRandom(min, max) {
+const getRandom = (min, max) => {
   let rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
-function getRandomFloat(min, max) {
-  return Math.random() * (max - min) + min;
+const photosArrayGenerate = () => {
+  const photos = [];
+  for (let index = 0; index < getRandom(MIN_IMAGE_COUNT, MAX_IMAGE_COUNT); index++) {
+    photos.push(`img/${photosArray[getRandom(0, photosArray.length - 1)]}`);
+  }
+  return photos;
 }
+
+const cards = [];
+
+for (let i = 0; i < COUNT_CARDS; i++) {
+  cards.push({
+    name: nameArray[getRandom(0, nameArray.length - 1)],
+    description: descriptionArray[getRandom(0, descriptionArray.length - 1)],
+    price: getRandom (MIN_PRICE, MAX_PRICE),
+    category: CATEGORY,
+    seller: {
+      fullname: fullNameArray[getRandom(0, fullNameArray.length - 1)],
+      rating: getRandom(MIN_SELLER_RATING, MAX_SELLER_RATING * 10) / 10
+    },
+    photos: photosArrayGenerate(),
+    address: {
+      city: cityArray[getRandom(0, cityArray.length - 1)],
+      street: streetArray[getRandom(0, streetArray.length -1)],
+      building: getRandom(ADDRESS_MIN_BUILDING, ADDRESS_MAX_BUILDING)
+    },
+    filters: {
+      type: typeArray[getRandom(0, typeArray.length - 1)],
+      area: getRandom(MIN_AREA, MAX_AREA),
+      'rooms-count': getRandom(MIN_ROOMS_COUNT, MAX_ROOMS_COUNT)
+    },
+    'publish-date': new Date()
+  });
+}
+
+console.log(cards);
